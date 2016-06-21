89f68197ae041c15f0446fb3d2283c14af68d1af
chunghuan.chang <chunghuan.chang@RTWPC10064>
2016-06-21 17:58:08 +0800

    加入專案檔案。

---
+++ ConvertFileName/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConvertFileName
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader table = new StreamReader(@".\table.csv");
+            string line;
+            while (table.EndOfStream)
+            {
+                line = table.ReadLine();
+                string[] filenames = line.Split(',');
+                System.IO.File.Move(filenames[0] + ".epub", filenames[1] + ".epub");
+                //System.IO.File.Move(filenames[0] + ".jpg", filenames[1] + ".jpg");
+            }
+        }
+    }
+}
